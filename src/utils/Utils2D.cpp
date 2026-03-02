b47c97f6f36f9c419fe1cd195b06b4114f094c4e
Lucas Ruiz Belmonte <kaekaes10@gmail.com>
2026-03-02 11:13:58 +0100

    Format

--- src/utils/Utils2D.cpp
+++ src/utils/Utils2D.cpp
@@ -4,21 +4,21 @@
 #include <glm/glm.hpp>
 
 glm::vec2 Utils2D::PercentToWorld(
-    float percentX,
-    float percentY,
-    int framebufferWidth,
-    int framebufferHeight,
-    const Camera& camera)
+	float percentX,
+	float percentY,
+	int framebufferWidth,
+	int framebufferHeight,
+	const Camera &camera)
 {
-    float aspect = framebufferHeight == 0
-        ? 1.0f
-        : static_cast<float>(framebufferWidth) / static_cast<float>(framebufferHeight);
+	float aspect = framebufferHeight == 0
+					   ? 1.0f
+					   : static_cast<float>(framebufferWidth) / static_cast<float>(framebufferHeight);
 
-	float halfHeight = -camera.orthoHeight * 0.5f;
-	float halfWidth = -halfHeight * aspect;
+	float halfHeight = camera.orthoHeight * 0.5f;
+	float halfWidth = halfHeight * aspect;
 
-    float worldX = -halfWidth + percentX * (2.0f * halfWidth);
-    float worldY = -halfHeight + percentY * (2.0f * halfHeight);
+	float worldX = -halfWidth + percentX * (2.0f * halfWidth);
+	float worldY = halfHeight - percentY * (2.0f * halfHeight);
 
-    return { worldX, worldY };
+	return {worldX, worldY};
 }
